7a4160d9b469299cd5cd473b3999456a401dcca4
yashmankar1 <yashmankar349@gmail.com>
2026-05-25 16:24:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="">
+        <h1 className="text-2xl font-bold text-center py-15">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
